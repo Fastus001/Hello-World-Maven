42955e2b53570a185e07eeebe22e75056ef694dd
Fastus001 <tkarczmarzyk@gmail.com>
2020-12-31 12:59:08 +0100

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by Tom - 31.12.2020
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(StringUtils.capitalize("hello world"));
+    }
+}
