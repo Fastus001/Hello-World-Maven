b0a9c7eaf2f5893cb47e21cf69da49b1db7b38eb
Fastus001 <tkarczmarzyk@gmail.com>
2021-01-01 18:43:08 +0100

    Added gitignore from GURU

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Tom - 31.12.2020
+ */
+public class HelloWorldTest {
+
+    public void testHello(){
+        System.out.println("Hello....");
+    }
+
+    public void testWorld(){
+        System.out.println("Hello - world");
+    }
+}
